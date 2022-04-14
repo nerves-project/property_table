b292e7853fb641beff32f04d32b75ae81bc9b202
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-14 19:33:26 -0400

    Add package metadata and update doc generation

--- mix.exs
+++ mix.exs
@@ -1,14 +1,19 @@
 defmodule PropertyTable.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/jjcarstens/property_table"
+
   def project do
     [
       app: :property_table,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
+      package: package(),
+      docs: docs(),
       dialyzer: dialyzer(),
       preferred_cli_env: %{
         docs: :docs,
@@ -40,11 +45,27 @@ defmodule PropertyTable.MixProject do
     "In-memory key-value store with subscriptions"
   end
 
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"Github" => @source_url}
+    ]
+  end
+
   defp dialyzer() do
     [
       flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs],
       list_unused_filters: true,
       plt_file: {:no_warn, "_build/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
