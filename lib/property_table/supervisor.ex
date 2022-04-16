673392210cb5e0d5d649142b3b8aa4a5e1a18a2f
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-16 15:44:50 -0400

    Standardize all GenServer naming on the table name
    This moves the public API for starting a PropertyTable completely to
    `property_table.ex`. It also changes the names in the process tree so
    that users are adding to their namespace rather than PropertyTable's. It
    looks like this:
    
    ```
    <table_name>
    <table_name>.Subscribers
    <table_name>.Table
    ```
    
    The `<table_name>` process is the supervisor. If the user calls
    `GenServer.stop` on it, everything goes down like you'd expect. This is
    different from before. Previously the PropertyTable supervisor was
    unnamed.
    
    All parameter validation is in `PropertyTable.start_link/1`. The code in
    `PropertyTable.Supervisor` is minimal now and it's been removed from the
    public API.

--- lib/property_table/supervisor.ex
+++ lib/property_table/supervisor.ex
@@ -1,51 +1,22 @@
 defmodule PropertyTable.Supervisor do
-  @moduledoc """
-  Supervisor for using a PropertyTable
-
-  To create a PropertyTable for your application or library, add the following
-  `child_spec` to one of your supervision trees:
-
-  ```elixir
-  {PropertyTable.Supervisor, name: MyTableName}
-  ```
-
-  The `:name` option is required. All calls to `PropertyTable` will need to
-  know it. Other options are:
-
-  * `:properties` - a list of `{property, value}` tuples to initially populate
-  the `PropertyTable`
-  """
+  @moduledoc false
   use Supervisor
-
-  @doc """
-  Manually start a PropertyTable Supervisor
-
-  Normally you should add a `child_spec` to your supervision tree to create a
-  new `PropertyTable`.
-  """
-  @spec start_link([PropertyTable.option()]) :: Supervisor.on_start()
-  def start_link(options) do
-    name = Keyword.get(options, :name)
-
-    unless !is_nil(name) and is_atom(name) do
-      raise ArgumentError, "expected :name to be given and to be an atom, got: #{inspect(name)}"
-    end
-
-    Supervisor.start_link(__MODULE__, options)
-  end
 
   @impl Supervisor
   def init(options) do
-    name = Keyword.fetch!(options, :name)
-    properties = Keyword.get(options, :properties, [])
-    registry_name = registry_name(name)
-    updated_options = Keyword.put(options, :registry_name, registry_name)
+    registry_name = registry_name(options.table)
 
-    PropertyTable.Table.create_ets_table(name, properties)
+    table_options = %{
+      registry: registry_name,
+      table: options.table,
+      tuple_events: options.tuple_events
+    }
+
+    PropertyTable.Table.create_ets_table(options.table, options.properties)
 
     children = [
-      {PropertyTable.Table, updated_options},
-      {Registry, [keys: :duplicate, name: registry_name]}
+      {PropertyTable.Table, table_options},
+      {Registry, [keys: :duplicate, name: registry_name, partitions: 1]}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
@@ -54,6 +25,6 @@ defmodule PropertyTable.Supervisor do
   @doc false
   @spec registry_name(PropertyTable.table_id()) :: Registry.registry()
   def registry_name(name) do
-    Module.concat(PropertyTable.Registry, name)
+    Module.concat(name, Subscriptions)
   end
 end
